310337286bbcb7847b116ecc7ef19ee8de0f06c4
jcgmU <tu_email@gmail.com>
2026-02-27 13:41:21 -0500

    chore(prisma): agregar migración inicial de base de datos

---
+++ prisma/migrations/20260227184030_init/migration.sql
@@ -0,0 +1,20 @@
+-- CreateTable
+CREATE TABLE "users" (
+    "id" UUID NOT NULL,
+    "email" VARCHAR(255) NOT NULL,
+    "password" VARCHAR(255) NOT NULL,
+    "name" VARCHAR(100) NOT NULL,
+    "is_active" BOOLEAN NOT NULL DEFAULT true,
+    "created_at" TIMESTAMPTZ NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updated_at" TIMESTAMPTZ NOT NULL,
+    "password_reset_token" VARCHAR(255),
+    "password_reset_expires" TIMESTAMPTZ,
+
+    CONSTRAINT "users_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "users_email_key" ON "users"("email");
+
+-- CreateIndex
+CREATE INDEX "users_email_idx" ON "users"("email");
